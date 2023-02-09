df566b1a595301eec47c8ee29f4184cbd6ccf81f
mike <matillman95@gmail.com>
2023-02-08 20:42:59 -0700

    contextAPI

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
+import React from 'react'
+import { StateContext } from '../context/StateContext'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return(
+    <StateContext>
+      <Toaster/>
+      <Component {...pageProps} />
+    </StateContext>
+    )
+  }
