c20a564475fea9404e7786bb3ae4e1c81eeaf8da
mike <matillman95@gmail.com>
2023-04-04 12:20:02 -0700

    stripe setup

--- pages/Shop.js
+++ pages/Shop.js
@@ -22,8 +22,6 @@ export const getServerSideProps = async () => {
     const query = '*[_type == "product"]'
     const products = await client.fetch(query)
 
-    // const bannerQuery = '*[_type =="banner"]'
-    // const bannerData = await client.fetch(bannerQuery)
 
     return {
         props: {products}
